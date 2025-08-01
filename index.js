1e317e8f1b40e1ef59d8943917fa268758346b6e
Tadeu <josetadeundasilva@gmail.com>
2025-07-31 22:06:41 -0300

    nova versao teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API funcionando!");
+  res.send(" Nova versão da API funcionando!");
 });
 
 app.listen(3000, () => {
